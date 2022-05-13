912ae477d011e8b61cae3f5e74ba47df5ac7f4c7
TheTobiGit <aemonsarfo@outlook.com>
2022-05-13 21:11:56 +0000

    update

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let count = 1;
-let amtEl = document.getElementById("amt-el");
+let count1 = 1;
+let count2 = 1;
+let amtEl = document.getElementById("amt-el1");
+let amtEl2 = document.getElementById("amt-el2");
+
 
 function increment() {
-  count ++;
-  amtEl.textContent = count;
+  count1 ++;
+  amtEl.textContent = count1;
 }
 
 function decrement() {
-  count --;
-  amtEl.textContent = count;
+  count1 --;
+  amtEl.textContent = count1;
+}
+
+function increment2() {
+  count2 ++;
+  amtEl2.textContent = count2;
+}
+
+function decrement2() {
+  count2 --;
+  amtEl2.textContent = count2;
 }
